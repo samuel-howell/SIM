98d18911f2ab9120797d61b4c54d4a792a0d6959
Samuel Howell <60439628+SlideHowell@users.noreply.github.com>
2021-08-25 18:36:17 -0400

    added custom colors and a test logo

---
+++ lib/src/res/custom-colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color cgray = Color(0xff403F4C);
+  static final Color cred = Color(0xffE84855);
+  static final Color cyellow = Color(0xffE84855);
+  static final Color cblue = Color(0xff3185FC);
+  static final Color cpink = Color(0xffEFBCD5);
+}
+
+
+/* Color scheme for app could be
+ 0xff403F4C -dark liver,  
+ 0xffE84855 -red crayola,  
+ 0xffF9DC5C -naples yellow,  
+ 0xff3185FC -azure,  
+ 0xffEFBCD5 -cameo pink
+
+*/
--- lib/src/screens/create-screen.dart
+++ lib/src/screens/create-screen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:howell_capstone/src/res/custom-colors.dart';
+
+
 
 class CreateScreen extends StatelessWidget {
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Create Screen'),
         centerTitle: true,
-        backgroundColor: Colors.purple
+        backgroundColor: Colors.black
       )
     );
   }
 }
-
 //TODO: createa form on this page to take in info to put into the firestore database. modify what is in this video to work with the form https://www.youtube.com/watch?v=lyZQa7hqoVY 
